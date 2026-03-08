6ef7b5906d4918f80bc7ed65932fdff5f16a0fcc
Kyle Finley <kyle@kylefinley.net>
2026-03-07 23:59:20 -0800

    updated: copy

--- packages/vue-client/src/views/Shoe-Jam.vue
+++ packages/vue-client/src/views/Shoe-Jam.vue
@@ -7,16 +7,16 @@
       On most Wednesdays you can find
       <a href="https://kylefinley.net" target="new">Kyle Finley</a> running the Dirty Works Jazz Jam
       at <a href="https://www.theluckyhorseshoebar.com" target="new">The Lucky Horseshoe</a> at 453
-      Cortland Ave. in San Francisco's Bernal Heights neighborhood. This is an open jam from 8 to
-      11 PM every 2nd through 5th Wednesdays of the month.
+      Cortland Ave. in San Francisco's Bernal Heights neighborhood. This is an open jam from 8 to 11
+      PM every 2nd through 5th Wednesdays of the month.
     </p>
     <p>
-      "The Shoe" (as it's known by the locals) has a long history of live music. Before becoming the
-      Lucky Horseshoe the bar was Skip's Tavern, and before that it was The Cherokee. Skip's was
-      known for a thriving blues scene. On the first Wednesday of the month you'll find a blues jam
-      at The Shoe run by some of the old Skip's crew. Live music has been played at this bar since
-      it opened in the 40s and there are rumors that Charlie Parker played the stage once but
-      there's no documented evidence of this.
+      "The Shoe" has a long history of live music. Before it became the Lucky Horseshoe the bar was
+      called Skip's Tavern, and before that it was The Cherokee. Skip's was known for a thriving
+      blues scene and nightly jams. On the first Wednesday of the month you'll find a blues jam at
+      The Shoe run by some of the old Skip's crew. Live music has been played at this bar since it
+      opened in the 40s and there are rumors that Charlie Parker played the stage once but there's
+      no documented evidence of this.
     </p>
     <p>
       The Shoe Jazz Jam has been running strong since March 2023. This is an open jam so all level
