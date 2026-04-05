6fd533eff6c605c364981344db4ca25516fd717f
Kyle Finley <kyle@kylefinley.net>
2026-04-04 17:10:11 -0700

    updated: burlesque jam copy

--- packages/vue-client/src/views/Burlesque-Jam.vue
+++ packages/vue-client/src/views/Burlesque-Jam.vue
@@ -4,20 +4,25 @@
       <h2>Burlesque Jazz Jam @ Paris 75</h2>
     </div>
     <p>
-      On the 1st Thursday of the month
+      On the 1st & 3rd Thursdays of the month
       <a href="https://kylefinley.net" target="new">Kyle Finley</a> and the Dirty Works Jazz Band
-      host San Francisco's only Burlesque Show backed by live jazz in historic North Beach at
-      <a href="https://lppsf.com" target="paris">Paris 75</a>, located at 515 Broadway, from 8 to 11
-      PM.
+      host San Francisco's only regular burlesque show backed by a live jazz band in historic North
+      Beach at <a href="https://lppsf.com" target="paris">Paris 75</a>, located at 515 Broadway,
+      from 8 to 11 PM.
     </p>
     <p>
-      The Dirty Works Burlesque Jam features Raya dancing several acts while the band plays. We
-      continue to jam between dance performances and keep the party going all night!
+      The Dirty Works Burlesque Jam features one of our burlesque dancers performing several acts
+      while the band plays. Dance performances are usually during the 9 o'clock set. We continue to
+      jam between dances and keep the party going all night!!!
     </p>
     <p>
-      This is a straight ahead jazz jam so classic jazz standards can be expected. There's no need to sign up
-      for this jam, simply bring your axe and jump in with the band. Singers are welcome too! If you have any questions reach
-      out to <a href="mailto:kyle@kylefinley.net">Kyle Finley</a>
+      This is a straight ahead open jam so we welcome players to join us. We play classic jazz
+      standards leaning heavy into the bebop era. There's no need to sign up for this jam, simply
+      bring your axe and jump in with the band. Singers are welcome too! If you have any questions
+      reach out to <a href="mailto:kyle@kylefinley.net">Kyle Finley</a>
+    </p>
+    <p>
+      Any dancers interested in performing at the show come to a burlesque jam night and let's talk!
     </p>
   </main>
 </template>
