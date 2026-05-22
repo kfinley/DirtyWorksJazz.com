7d1e279b3c5f8e1900733e3df585fccd1ce64821
Kyle Finley <kyle@kylefinley.net>
2026-05-22 10:44:06 -0700

    updated: removed gig. updated burlesque jam page

--- packages/vue-client/src/views/Burlesque-Jam.vue
+++ packages/vue-client/src/views/Burlesque-Jam.vue
@@ -4,7 +4,7 @@
       <h2>Burlesque Jazz Jam @ Paris 75</h2>
     </div>
     <p>
-      On the 1st & 3rd Thursdays of the month
+      Every Thursdays
       <a href="https://kylefinley.net" target="new">Kyle Finley</a> and the Dirty Works Jazz Band
       host San Francisco's only regular burlesque show backed by a live jazz band in historic North
       Beach at <a href="https://lppsf.com" target="paris">Paris 75</a>, located at 515 Broadway,
@@ -12,11 +12,11 @@
     </p>
     <p>
       The Dirty Works Burlesque Jam features one of our burlesque dancers performing several acts
-      while the band plays. Dance performances are usually during the 9 o'clock set. We continue to
+      while the band plays. Dance performances are during the 9 o'clock set. We continue to
       jam between dances and keep the party going all night!!!
     </p>
     <p>
-      This is a straight ahead open jam so we welcome players to join us. We play classic jazz
+      This is a straight ahead open jam and we welcome players to join us. We play classic jazz
       standards leaning heavy into the bebop era. There's no need to sign up for this jam, simply
       bring your axe and jump in with the band. Singers are welcome too! If you have any questions
       reach out to <a href="mailto:kyle@kylefinley.net">Kyle Finley</a>
